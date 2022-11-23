2326343a2b62af1b51303c0f9419c5fb1bcee78b
Rodolfo Alvarez <atahualpa1970@outlook.com>
2022-11-23 13:29:31 -0300

    app route /

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+app.use('/', (req, res) => res.send('API DOMUS20'))
 app.use('/api/reclamos', require('./routes/reclamo.routes'))
 app.use('/api/props', require('./routes/prop.routes'))
 app.use('/api/clientes', require('./routes/cliente.routes'))
